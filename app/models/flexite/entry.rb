f8f03ff5333c3fcc4f40a1e8232ddaf90aac4f94
mrusakovich <rusakovich.maksim@gmail.com>
2018-05-04 10:18:30 +0300

    make table_name instance accessible

--- app/models/flexite/entry.rb
+++ app/models/flexite/entry.rb
@@ -1,6 +1,7 @@
 class Flexite::Entry < ActiveRecord::Base
   belongs_to :entry, polymorphic: true
   attr_accessible :type, :value
+  delegate :table_name, to: 'self.class'
 
   def cast_v1
     t1.constantize.cast(v1)
@@ -8,11 +9,5 @@ class Flexite::Entry < ActiveRecord::Base
 
   def cast_v2
     t2.constantize.cast(v2)
-  end
-
-  private
-
-  def table_name
-    self.class.table_name
   end
 end
