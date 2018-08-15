054eb4f3b56353a9801d8078423086a703636942
mrusakovich <rusakovich.maksim@gmail.com>
2018-08-15 19:02:21 +0300

    use lcs algorithm for array comparison update gem version

--- app/services/flexite/config/update_service.rb
+++ app/services/flexite/config/update_service.rb
@@ -6,7 +6,7 @@ module Flexite
           return failure
         end
 
-        @record = Config.joins(:entry)
+        @record = Config.joins("LEFT JOIN #{Entry.table_name} ON #{Entry.table_name}.parent_id = #{Config.table_name}.id AND #{Entry.table_name}.parent_type = '#{Config.model_name}'")
           .select("#{Config.table_name}.*, #{Entry.table_name}.id AS entry_id")
           .where(id: @form.id).first
         @record.update_attributes(@form.attributes)
--- app/services/flexite/diff/sync_check_service.rb
+++ app/services/flexite/diff/sync_check_service.rb
@@ -20,41 +20,22 @@ module Flexite
           return { error: 'Invalid token', code: 401 }
         end
 
-        diffs = HashDiff.diff(@current_tree, @other_tree, array_path: true, use_lcs: false)
+        diffs = HashDiff.diff(@current_tree, @other_tree, array_path: true)
 
         if diffs.blank?
           return {}
         end
 
         view_diffs = { '+': [], '-': [], '~': [] }
-        @patched = HashDiff.patch!(@current_tree, diffs)
 
         diffs.each do |type, depth, *changes|
-          view_diffs[type.to_sym] << [get_depth(type.to_sym == :- ? depth.slice(0..-2) : depth), *changes]
+          view_diffs[type.to_sym] << [depth, *changes]
         end
 
         Flexite.cache.write("#{Flexite.state_digest}-#{@checksum}-#{@stage}-diffs", diffs)
         { diffs: view_diffs }
       rescue => exc
         { error: exc.message, code: 500 }
-      end
-
-      private
-
-      def get_depth(depth)
-        node = @patched
-
-        view_depth = depth.each.with_object([]) do |level, memo|
-          node = node[level]
-
-          case node
-            when ::Hash
-              next if node['name'].blank?
-              memo << node['name']
-          end
-        end
-
-        view_depth.join(Flexite.config.diff_depth_separator)
       end
     end
   end
